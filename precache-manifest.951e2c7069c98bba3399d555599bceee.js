249b3a2d767d27a689472a55559f43c9e5b6f489
lana wong <lana_1123@hotmail.com>
2020-06-08 16:46:54 +0800

    Updates

---
+++ precache-manifest.951e2c7069c98bba3399d555599bceee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70780b5b73681da60a5c5743927fdecb",
+    "url": "/foodRecommendationBasicDemo/index.html"
+  },
+  {
+    "revision": "c28ed46a15bdc7c5a879",
+    "url": "/foodRecommendationBasicDemo/static/css/main.24c4fa0e.chunk.css"
+  },
+  {
+    "revision": "7c7f05c7ce34c7e3184a",
+    "url": "/foodRecommendationBasicDemo/static/js/2.91c7cb17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/foodRecommendationBasicDemo/static/js/2.91c7cb17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28ed46a15bdc7c5a879",
+    "url": "/foodRecommendationBasicDemo/static/js/main.67a124c1.chunk.js"
+  },
+  {
+    "revision": "96843086a7fd8b2e781f",
+    "url": "/foodRecommendationBasicDemo/static/js/runtime-main.db8c0423.js"
+  }
+]);
